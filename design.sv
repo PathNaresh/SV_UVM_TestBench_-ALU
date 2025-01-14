626ec4ad297b59624372f9859037d0bacc3a0b70
Pathakamuri Naresh <123228297+PathNaresh@users.noreply.github.com>
2025-01-14 21:58:28 +0530

    design.sv

--- design.sv
+++ design.sv
@@ -1,43 +1,25 @@
 
 module my_dut(
+  input       clock,
+  input       reset,
+  input [2:0] a, b,
+  input       sel,
   
-  clock,
-  reset,
-  a,
-  b,
-  sel,
-  result
-  
+  output reg [3:0] result
 );
-  
-  input clock;
-  input  reset;
-  input [2:0] a, b;
-  input sel;
-  
-  output reg [3:0] result;
-    
+
   always @(posedge clock) begin
     
     if(reset) begin
-      
       result = 0;
-      
     end 
-    
     else begin
-      
-      case(sel)
-        
+      case(sel)  
         1'b0: result = a + b; 
         1'b1: result = a - b;
-      
         default: result = 0; 
-      
       endcase
-    
-    end
-    
+    end  
   end
 
 endmodule
